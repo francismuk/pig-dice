9d2622bdfd31dc208613c9516bd5f8d7fc60e39d
Francis Mukuha <mukuha58@gmail.com>
2019-05-10 17:39:41 +0300

    add buttons for roll and restart

--- js/scripts.js
+++ js/scripts.js
@@ -15,7 +15,26 @@ $(document).ready(function() {
 
 var iRandomNumber;
 var iRollDice;
-var i;
+var scores;
+var roundScore;
+var activePlayer;
+var gamePlaying;
+
+function init() {
+  scores = [0,0];
+  activePlayer = 0;
+  roundScore = 0;
+  gamePlaying = true;
+
+  document.getElementById('dice-1').style.display = 'none';
+}
+
+
+
+
+
+
+
 
 iRandomNumber = Math.ceil(Math.random()*6);
 
